6a18ce4fe3bd4864f8a1c807a9e00c74a95519c1
mafuba <tulkidg@gmail.com>
2024-02-29 21:25:02 +0300

    fix: add unique constraint to email

--- backend/src/main/java/ru/nasrulaev/tasktrackerbackend/model/User.java
+++ backend/src/main/java/ru/nasrulaev/tasktrackerbackend/model/User.java
@@ -16,7 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column
+    @Column(unique = true)
     @NotBlank
     @Email
     @Size(max = 320)
