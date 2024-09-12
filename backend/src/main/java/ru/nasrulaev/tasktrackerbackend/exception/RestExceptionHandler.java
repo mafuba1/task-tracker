b9ba7ce7fc26f84bef37b2257ba22d182ca6072b
mafuba <tulkidg@gmail.com>
2024-09-12 22:54:04 +0300

    refactor(backend): remove unused import statements

--- backend/src/main/java/ru/nasrulaev/tasktrackerbackend/exception/RestExceptionHandler.java
+++ backend/src/main/java/ru/nasrulaev/tasktrackerbackend/exception/RestExceptionHandler.java
@@ -1,7 +1,6 @@
 package ru.nasrulaev.tasktrackerbackend.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
