2ed3ebd8dfd24eee784ed09ccb15d4d4170327d8
mafuba <tulkidg@gmail.com>
2024-09-12 22:48:56 +0300

    fix(backend): add no-argument constructor to GetUserInfoResponse

--- backend/src/main/java/ru/nasrulaev/tasktrackerbackend/dto/GetUserInfoResponse.java
+++ backend/src/main/java/ru/nasrulaev/tasktrackerbackend/dto/GetUserInfoResponse.java
@@ -1,4 +1,30 @@
 package ru.nasrulaev.tasktrackerbackend.dto;
 
-public record GetUserInfoResponse(long id, String email) {
+public class GetUserInfoResponse{
+    private long id;
+    private String email;
+
+    public GetUserInfoResponse() {
+    }
+
+    public GetUserInfoResponse(long id, String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
